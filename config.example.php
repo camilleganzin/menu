e4bc976004d9a2730bcfa75b79d054fe715630ae
camilleganzin <camilleganzin@gmail.com>
2017-11-04 14:13:49 +0100

    remove jquery-2.1.1 and update config example file

--- config.example.php
+++ config.example.php
@@ -4,3 +4,6 @@
 	$config['dsn'] = 'mysql:dbname=***;host=****';
 	$config['user'] = '***';
 	$config['password'] = '***';
+	$config['salt']= '***';
+	$config['dossier_images'] = 'images';
+	$config['ajax_url'] = 'ajax_admin.php';
