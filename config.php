d0380071dc780958f5f507b65a6b8fe4c220ec63
camilleganzin <camilleganzin@gmail.com>
2017-11-04 13:48:54 +0100

    remove unnecessary files

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
 	$config = array();
-	// $config['dsn'] = 'mysql:dbname=camilleganzin;host=mysql51-45.perso;charset=utf8';
-	// $config['user'] = 'camilleganzin';
-	// $config['password'] = 'niKxPjN2';
-	$config['dsn'] = 'mysql:dbname=menu;host=localhost;charset=utf8';
-	$config['user'] = 'root';
-	$config['password'] = 'root';
+	$config['dsn'] = 'mysql:dbname=recetteshncg;host=recetteshncg.mysql.db;charset=utf8';
+	$config['user'] = 'recetteshncg';
+	$config['password'] = 'ceciestuntestpour3wA_44*'; 
+	// $config['dsn'] = 'mysql:dbname=menu;host=localhost;charset=utf8';
+	// $config['user'] = 'root';
+	// $config['password'] = 'root';
 	$config['admin_password'] = 'e5214a9cbd2304b7b76735f645d42e157c152a7fde8c943f08242bba40719cd81f74cc40512488f885f23a2a67a5ee8320dff960e589e49bcc2dd83a34277c63';
 	$config['salt']= 'Xy&<%L+MoI@gw|)3#hbt`E*Yw';
 	$config['dossier_images'] = 'images';
